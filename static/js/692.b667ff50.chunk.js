fe581dd883eaca883f1b95812146dd7428c7c8d8
Rostykfps <105645961+Rostykfps@users.noreply.github.com>
2023-08-20 13:06:13 +0000

    Deploying to gh-pages from @ Rostykfps/goit-react-hw-08-phonebook@85bdcccdb212449f7a29515c445190b27a0d4faa 🚀

---
+++ static/js/692.b667ff50.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[692],{7692:function(t,e,h){h.d(e,{qyE:function(){return o}});var n=h(9983);function o(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2 7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z"}}]})(t)}}}]);
+//# sourceMappingURL=692.b667ff50.chunk.js.map
